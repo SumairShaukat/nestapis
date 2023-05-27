c8e22dc9a7d25059794df0ecd72d92fbb38a2ed5
SumairShaukat <sumairch224@gmail.com>
2023-05-27 21:20:03 +0500

    add dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,17 +4,18 @@ import {
   Post,
   Req,
 } from '@nestjs/common';
+import { AuthDto } from './dto';
 import { AuthService } from './auth.service';
-import { AuthDto } from 'src/dto/auth.dto';
-
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
   signup(@Body() dto: AuthDto) {
-    console.log({ dto });
-    return this.authService.login();
+    console.log({
+      dto,
+    });
+    return this.authService.signin();
   }
 
   @Post('signin')
--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -2,11 +2,12 @@ import {
   IsEmail,
   IsNotEmpty,
   IsInt,
+  IsString,
 } from 'class-validator';
 export class AuthDto {
   @IsEmail()
   @IsNotEmpty()
   email: string;
-  @IsInt()
-  password: number;
+  @IsString()
+  password: String;
 }
