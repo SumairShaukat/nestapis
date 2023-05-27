e081089a96b8fb8deb20a8eadd9a3d9fabaecf66
SumairShaukat <sumairch224@gmail.com>
2023-05-27 18:43:40 +0500

    intial commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('APi docs')
+    .setDescription('The Median Api Description')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3000);
+}
+bootstrap();
